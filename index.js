b1b64f14f6278c1ecd31c9bd492549b77d05f99d
Lyssa Treibitz <cerylean@gmail.com>
2018-06-17 17:20:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ function destructivelyRemoveFirstKitten() {return kittens.shift()}
 
 function destructivelyRemoveLastKitten() {return kittens.pop()}
 
-function appendKitten(name) {array=[element,...array]; return array}
+function appendKitten(name) {newKittens=[name,...kittens]; return newKittens}
