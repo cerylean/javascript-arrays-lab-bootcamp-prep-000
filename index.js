0debb4485fdcd8b71afc12008376f0e3b2301000
Lyssa Treibitz <cerylean@gmail.com>
2018-06-17 17:20:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much."
+
+function destructivelyPrependKitten(name) {return kittens.unshift(name)}
+
+function destructivelyAppendKitten(name) {return kittens.push(name)}
+
+function destructivelyRemoveFirstKitten() {return kittens.shift()}
+
+function destructivelyRemoveLastKitten() {return kittens.pop()}
+
+function addElementToBeginningOfArray(array, element) {array=[element,...array]; return array}
